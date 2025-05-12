3abdd944fb34ab2bad4dfeb3fd7c0cd820d0ebb2
Sina Parsa <Sina.P64@Outlook.com>
2025-05-12 22:35:22 +0330

    Add files via upload

---
+++ Example.py
@@ -0,0 +1,19 @@
+x = 23 + 45 ## addition operator
+
+y = 45 - 23 ## subtraction operator
+
+a = 23 * 2 ## multiplication operator
+
+b = 2 ** 4 ## power operator
+
+#float
+
+c = 60 / 5 ## division operator
+
+#integer
+
+d = 70 // 3 ## floor division operator
+
+f = 56 % 3 ## modul operator
+
+print(x , y , a , b , c , d , f)
---
+++ Parentesforskel.py
@@ -0,0 +1,5 @@
+x = 10 * (5 + 3)
+
+y = 10 * 5 + 3
+
+print (x , type(x) , y , type(y))
